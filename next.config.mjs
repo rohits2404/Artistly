be832eae9d175f96a853dea0cdeb764d45c6854c
Rohit Sharma <rohits8731@gmail.com>
2025-06-24 12:13:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,15 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    domains: ["placeholder.svg"],
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        domains: ["placeholder.svg"],
+        unoptimized: true,
+    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
